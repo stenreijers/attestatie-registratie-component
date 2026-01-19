6f18af40779f52b62bfdec72762e34a513ca308f
Marnix Dessing <m.dessing@nijmegen.nl>
2026-01-19 14:40:51 +0100

    chore: make sure we can publish

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,10 +2,10 @@ import { GemeenteNijmegenTsPackage } from '@gemeentenijmegen/projen-project-type
 const project = new GemeenteNijmegenTsPackage({
   defaultReleaseBranch: 'main',
   devDeps: ['@gemeentenijmegen/projen-project-type'],
-  name: 'AttestatieRegestratieComponent',
+  name: '@gemeentenijmegen/attestatie-registratie-component',
   projenrcTs: true,
   repository: 'https://github.com/GemeenteNijmegen/attestatie-registratie-component',
-  npmTrustedPublishing: false, // TODO fix the temp token used currently
+  npmTrustedPublishing: true,
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */

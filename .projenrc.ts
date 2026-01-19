df1169bd2af9bb4b16a12a367ca3ec2bcb93dd19
Marnix Dessing <m.dessing@nijmegen.nl>
2026-01-19 14:07:16 +0100

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,13 @@
+import { GemeenteNijmegenTsPackage } from '@gemeentenijmegen/projen-project-type';
+const project = new GemeenteNijmegenTsPackage({
+  defaultReleaseBranch: 'main',
+  devDeps: ['@gemeentenijmegen/projen-project-type'],
+  name: 'AttestatieRegestratieComponent',
+  projenrcTs: true,
+  repository: 'https://github.com/GemeenteNijmegen/attestatie-registratie-component',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
